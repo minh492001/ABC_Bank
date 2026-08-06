95a6a50d4d9b8dcb5523f156090dee044e284db6
MinMin <gow.bin.gg@gmail.com>
2026-08-06 15:55:23 +0700

    feature: add-Cors-Config

---
+++ src/main/java/com/abc/abcbank/security/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.abc.abcbank.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*"); // in production only allow origins from trusted frontend
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setMaxAge(3600L);
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
