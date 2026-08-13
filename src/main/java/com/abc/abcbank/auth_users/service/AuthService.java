f59a695b40a9aa1734591a58977be4340f1d8c44
MinMin <gow.bin.gg@gmail.com>
2026-08-13 17:43:01 +0700

    feat: create UpdatePassword feature

--- src/main/java/com/abc/abcbank/auth_users/service/AuthService.java
+++ src/main/java/com/abc/abcbank/auth_users/service/AuthService.java
@@ -3,6 +3,7 @@ package com.abc.abcbank.auth_users.service;
 import com.abc.abcbank.auth_users.dto.LoginRequest;
 import com.abc.abcbank.auth_users.dto.LoginResponse;
 import com.abc.abcbank.auth_users.dto.RegistrationRequest;
+import com.abc.abcbank.auth_users.dto.ResetPasswordRequest;
 import com.abc.abcbank.response.Response;
 
 public interface AuthService {
@@ -10,5 +11,6 @@ public interface AuthService {
     Response<String> register (RegistrationRequest request);
     Response<LoginResponse> login(LoginRequest loginRequest);
     Response<? > forgetPassword(String email);
+    Response<? > updatePasswordViaResetCode(ResetPasswordRequest resetPasswordRequest);
 
 }
