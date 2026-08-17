98721deecc322bbfd6ef0598117c2a403758fc08
MinMin <gow.bin.gg@gmail.com>
2026-08-17 09:15:48 +0700

    feat: create function getMyProfile in User Service

--- src/main/java/com/abc/abcbank/auth_users/service/UserService.java
+++ src/main/java/com/abc/abcbank/auth_users/service/UserService.java
@@ -1,8 +1,11 @@
 package com.abc.abcbank.auth_users.service;
 
+import com.abc.abcbank.auth_users.dto.UserDTO;
 import com.abc.abcbank.auth_users.entity.User;
+import com.abc.abcbank.response.Response;
 
 public interface UserService {
 
     User getCurrentLoggedInUser();
+    Response<UserDTO> getMyProfile();
 }
